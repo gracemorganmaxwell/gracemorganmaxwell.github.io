59a6dd23aedeffd401c8b35fd7eea8750e1ab106
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-08-07 17:54:35 +1200

    Initial commit - reset

--- 2024/web/src/components/LineSeparatorComponent/LineSeparatorComponent.tsx
+++
@@ -1,9 +0,0 @@
-const LineSeparatorComponent = () => {
-  return (
-    <div className="mb-6 flex justify-center">
-      <div className="w-[90vw] border-t border-darkBlue"></div>
-    </div>
-  )
-}
-
-export default LineSeparatorComponent
